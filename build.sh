dc467e0cb703e2f352447cc12b93246ac42b48a5
Tom Mitchell <tom@tom.org>
2022-10-25 18:38:19 -0400

    add latest tag on Docker push

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ VERSION=4.9.1
 
 docker buildx build --push --platform linux/amd64,linux/arm64,linux/arm/v7 -t mitct02/weewx:$VERSION .
 #docker push mitct02/weewx:$VERSION
-#docker tag mitct02/weewx:$VERSION mitct02/weewx:latest
-#docker push mitct02/weewx:latest
+docker tag mitct02/weewx:$VERSION mitct02/weewx:latest
+docker push mitct02/weewx:latest
