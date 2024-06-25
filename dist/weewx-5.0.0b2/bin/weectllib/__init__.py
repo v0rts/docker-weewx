d5a7d78539852662db2e5945930364a98c131d5b
Tom Mitchell <52498+tomdotorg@users.noreply.github.com>
2024-06-25 17:28:38 -0400

    dists

--- dist/weewx-5.0.0b2/bin/weectllib/__init__.py
+++
@@ -1,6 +0,0 @@
-#
-#    Copyright (c) 2023 Tom Keffer <tkeffer@gmail.com>
-#
-#    See the file LICENSE.txt for your rights.
-#
-"""The 'weectllib' package."""
--- dist/weewx-5.0.0b2/bin/weeutil/__init__.py
+++
@@ -1,6 +0,0 @@
-#
-#    Copyright (c) 2009-2015 Tom Keffer <tkeffer@gmail.com>
-#
-#    See the file LICENSE.txt for your full rights.
-#
-"""General utilities"""
