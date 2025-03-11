20606d331e47111eca8b42cdbd81c321f0ac7f47
tom <tom@tom.org>
2025-03-10 20:48:05 -0700

    v5.1.0-7 - just bake :latest into build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ IMAGE_VERSION=$WEEWX_VERSION-$REV
 #docker build --no-cache -t mitct02/weewx:$VERSION .
 BUILDKIT_COLORS="run=123,20,245:error=yellow:cancel=blue:warning=white" \
   docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  -t mitct02/weewx:$IMAGE_VERSION  -t mitct02/weewx:latest \.
+  -t mitct02/weewx:$IMAGE_VERSION \.
 #BUILDKIT_COLORS="run=123,20,245:error=yellow:cancel=blue:warning=white" docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t mitct02/weewx:$IMAGE_VERSION .
 #docker pull mitct02/weewx:$IMAGE_VERSION
+
+if [ $? -eq 0 ]; then
+    docker buildx imagetools create \
+        -t mitct02/weewx:latest \
+        mitct02/weewx:$IMAGE_VERSION
+fi
