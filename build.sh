fa7df598ab3f8c7958eafb4c2ee3c9aa604ab328
tom <tom@tom.org>
2025-03-11 07:00:09 -0700

    clean up commented code

--- build.sh
+++ build.sh
@@ -2,14 +2,9 @@
 REV=7
 WEEWX_VERSION=5.1.0
 IMAGE_VERSION=$WEEWX_VERSION-$REV
-#IMAGE_VERSION=latest
-#docker build --no-cache -t mitct02/weewx:$VERSION .
 BUILDKIT_COLORS="run=123,20,245:error=yellow:cancel=blue:warning=white" \
   docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
   -t mitct02/weewx:$IMAGE_VERSION \.
-#BUILDKIT_COLORS="run=123,20,245:error=yellow:cancel=blue:warning=white" docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t mitct02/weewx:$IMAGE_VERSION .
-#docker pull mitct02/weewx:$IMAGE_VERSION
-
 if [ $? -eq 0 ]; then
     docker buildx imagetools create \
         -t mitct02/weewx:latest \
